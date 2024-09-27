fd7d30134e147459b9e02c31af5bc05a1e503cc2
vratislavvv <vratko.private@gmail.com>
2024-09-27 07:57:29 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 500 }}>
+      <Typography variant="h5" gutterBottom>
+      h5. Heading
+      </Typography>
+    </Box>
+  )
+}
